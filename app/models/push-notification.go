09a7200f591ff2ae0975aaad5ce7f4bb85c7dae4
unknown <vahnxp@gmail.com>
2020-11-26 19:57:24 -0300

    test fix missing notification

--- app/models/push-notification.go
+++ app/models/push-notification.go
@@ -2,14 +2,14 @@ package models
 
 // PushNotification : push notification structure
 type PushNotification struct {
-	To           string
-	Notification NotificationInfo
-	Data         interface{}
+	To           string           `json:"to,omitempty"`
+	Notification NotificationInfo `json:"notification,omitempty"`
+	Data         interface{}      `json:"data,omitempty"`
 }
 
 // NotificationInfo : push notification info
 type NotificationInfo struct {
-	Title       string
-	Body        string
-	ClickAction string
+	Title       string `json:"title,omitempty"`
+	Body        string `json:"body,omitempty"`
+	ClickAction string `json:"click_action,omitempty"`
 }
