3e3d05f86d4105f637074a6ee9cf060ac74261bc
unknown <vahnxp@gmail.com>
2020-11-26 16:54:42 -0300

    fix getcontextkey arguments

--- app/utils/get-context-key.go
+++ app/utils/get-context-key.go
@@ -3,7 +3,7 @@ package utils
 import "github.com/gin-gonic/gin"
 
 // GetContextKey : get key from context safely
-func GetContextKey(context *gin.Context, key string, value interface{}) interface{} {
+func GetContextKey(context *gin.Context, key string) interface{} {
 	if context.Keys != nil {
 		return context.Keys[key]
 	}
