2a48426e8d8829b4b9c9c161db38462f9a2acc0e
unknown <vahnxp@gmail.com>
2020-11-26 18:57:36 -0300

    validate firebase url and apikey + header

--- app/service/push-notification-service.go
+++ app/service/push-notification-service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"errors"
 	"os"
 
 	"github.com/Tsuryu/arreglapp-commons/app/models"
@@ -9,12 +10,23 @@ import (
 
 // SendPushNotification : sends a message to the device
 func SendPushNotification(notification models.PushNotification) error {
+	apiKey := os.Getenv("API_KEY_FIREBASE")
+	if apiKey == "" {
+		return errors.New("Missing firebase apikey")
+	}
+
+	url := os.Getenv("API_FIREBASE_URL")
+	if url == "" {
+		return errors.New("Missing firebase url")
+	}
+
 	client := resty.New()
 	request := client.R()
 
 	notification.Notification.ClickAction = "FLUTTER_NOTIFICATION_CLICK"
 	request.SetBody(notification)
+	request.SetHeader("Authorization", apiKey)
 
-	_, err := request.Post(os.Getenv("API_FIREBASE_URL"))
+	_, err := request.Post(url)
 	return err
 }
